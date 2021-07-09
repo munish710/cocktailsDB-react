7528e04db45486a4f52aa38791c7dbb8365be0ce
munish710 <munish710@gmail.com>
2021-07-09 20:49:07 +0530

    setup error and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Cocktails DB allows you to search about various different kinds of cocktails available in the world and their ingredients.</p>
+      <p>True delight for a Cocktails lover! What's your favourite drink?</p>
+    </section>
   );
 };
 
