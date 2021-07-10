f63df5fbdd89d8cf2da03d75c7fc393f69f4874b
munish710 <munish710@gmail.com>
2021-07-10 23:38:51 +0530

    setup basic states in context.js

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -2,8 +2,19 @@ import React from "react";
 import Cocktail from "./Cocktail";
 import Loading from "./Loading";
 import { useGlobalContext } from "../context";
+import { getByTitle } from "@testing-library/react";
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">No cocktails matched your search term</h2>
+    );
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
