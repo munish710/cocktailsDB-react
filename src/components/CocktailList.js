9ef1f662a5bdb5d6a4b15a9fda0639411bf1deb3
munish710 <munish710@gmail.com>
2021-08-11 23:41:17 +0530

    code refactor and global styles changed

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Cocktail from "./Cocktail";
 import Loading from "./Loading";
-import { useGlobalContext } from "../context";
+import { useGlobalContext } from "../context/context";
 
 const CocktailList = () => {
   const { cocktails, loading } = useGlobalContext();
