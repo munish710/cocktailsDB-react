676965a7b2470df1a2d8a9c2b3066132c93b6f5a
munish710 <munish710@gmail.com>
2021-08-12 00:13:10 +0530

    about page refactor

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,24 @@
 import React from "react";
-
+import cocktails from "../assets/cocktails.svg";
 const About = () => {
   return (
-    <section className="section about-section">
-      <h1 className="section-title">about us</h1>
-      <p>Cocktails DB allows you to search about various different kinds of cocktails available in the world and their ingredients.</p>
-      <p>True delight for a Cocktails lover! What's your favourite drink?</p>
+    <section className="section page hero-center">
+      <div className="hero-image-container">
+        <img src={cocktails} alt="questions" className="hero-image" />
+      </div>
+      <div className="hero-info">
+        <h3>
+          Welcome to <span className="hero-text">CocktailDB</span>
+        </h3>
+        <p>
+          Cocktails DB allows you to search about various different kinds of
+          cocktails available in the world and their ingredients. True delight
+          for a Cocktails lover! What's your favourite drink?
+        </p>
+        {/* <button className="btn">
+          <Link to="/explore">Explore Now</Link>
+        </button> */}
+      </div>
     </section>
   );
 };
